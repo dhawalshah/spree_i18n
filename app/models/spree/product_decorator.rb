93d58bbef3c360c5c27904876b2629ff83157d17
Dhawal <dhawaljshah@gmail.com>
2013-11-08 10:44:02 +0800

    Update product_decorator.rb

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,7 +1,5 @@
 module Spree
   Product.class_eval do
-    translates :name, :description, :meta_description, :meta_keywords, :permalink,
-      :fallbacks_for_empty_translations => true
     include SpreeI18n::Translatable
   end
 end
